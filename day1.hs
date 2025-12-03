7f8c774464b05ca91f086106222ecee176f1827d
Mukhamed Karanashev <menchauser@gmail.com>
2025-12-03 09:16:05 +0100

    WIP: Day1

--- day1.hs
+++ day1.hs
@@ -3,7 +3,7 @@ import System.Environment
 
 
 -- Rotation is either left or right 
-type Rotation = Either Integer Integer
+type Rotation = Either Int Int
 
 parseRotation :: String -> Rotation
 parseRotation ('L' : cs) = Left (read cs)
@@ -19,22 +19,37 @@ readInput path =
      parseInput input 
 
 -- Now we have list of rotations and need to count number of zeros 
-rotate :: Integer -> Rotation -> Integer
+rotate :: Int -> Rotation -> Int
 rotate x (Left y)
   | y > 100 = rotate x (Left $ y `mod` 100)
   | x < y = x + (100 - y)
   | otherwise = x - y 
 rotate x (Right y) = (x + y) `mod` 100
 
-part1 :: [Rotation] -> Integer
+part1 :: [Rotation] -> Int
 part1 rs =
   let dialPositions = scanl rotate 50 rs
-  in toInteger $ length $ filter (== 0) dialPositions
+  in length $ filter (== 0) dialPositions
 
-debug1 :: [Rotation] -> [(Rotation, Integer)]
+debug1 :: [Rotation] -> [(Rotation, Int)]
 debug1 rs =
   let dialPositions = scanl rotate 50 rs
    in zip rs $ tail dialPositions
+
+-- Part 2
+-- If during rotation we got value bigger than 100 or less or equal to 0 - it is a click
+rotate2 :: (Int, Int) -> Rotation -> (Int, Int)
+rotate2 (state, clicks) (Left n) =
+  let newState = state - n
+  in (newState `mod` 100, clicks + abs (newState `div` 100))
+rotate2 (state, clicks) (Right n) = 
+    let newState = state + n
+    in (newState `mod` 100, clicks + abs (newState `div` 100))
+
+part2 :: [Rotation] -> Int
+part2 rs =
+  let dialPositions = scanl rotate 50 rs
+  in length $ filter (== 0) dialPositions
 
 main :: IO ()
 main = do
