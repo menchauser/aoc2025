7e6eae88fce38f472e3b690f3ab2509e1e58c6de
Mukhamed Karanashev <menchauser@gmail.com>
2025-12-09 20:41:51 +0100

    Day 06

--- day06.hs
+++ day06.hs
@@ -1,3 +1,4 @@
+import Data.Either
 import Data.Functor
 import Data.List
 import System.IO
@@ -41,17 +42,20 @@ testInput =
 hspace :: Parsec String u ()
 hspace = skipMany (oneOf " \t")
 
+hspace1 :: Parsec String u ()
+hspace1 = skipMany1 (oneOf " \t")
+
 numListP :: Parsec String u [Integer]
 numListP = do
-  optional hspace -- Line can start with spaces
-  sepBy1 (many1 digit <&> read) hspace
+  hspace
+  many1 ((many1 digit <&> read) <* hspace)
 
 opP = do
         c <- oneOf "*+"
         return $ read [c]
 
 opListP :: Parsec String u [Op]
-opListP = endBy1 opP hspace
+opListP = between hspace hspace $ endBy1 opP hspace
 
 inputP :: Parsec String u Input
 inputP = do
@@ -80,4 +84,37 @@ solve (Input ps ops) = sum $ zipWith calc ops ps
 
 runPart1 path = do
   input <- readInput path
+  return $ solve input
+
+
+-- Part 2
+{-
+For Part 2 we read input data differently.
+1. Read it into list of Strings: one for each input line.
+2. Take all lines but last one (containing operations) and transpose them.
+3. Transposed lines can be cleaned from whitespaces and split into groups. Split
+  is done by the empty column separator between them.
+4. Operations are parsed separately using Parsec.
+-}
+
+readInput2 path = do 
+  inh <- openFile path ReadMode
+  input <- hGetContents inh
+  let ls = lines input
+      plen = length ls - 1
+      pls = take plen ls
+      numstrings = map (filter (/= ' ')) $ transpose pls
+      nums = (map (map read) $ split numstrings "") :: [[Integer]]
+      opstring = head $ drop plen ls
+      ops = fromRight [] $ parse opListP path opstring
+    in return Input { problems = nums, operations = ops }
+
+-- split xs into list of lists separated by sep
+split :: (Eq a) => [a] -> a -> [[a]]
+split [] sep = []
+split xs sep = let (h, rest) = span (/= sep) xs
+               in h : split (drop 1 rest) sep
+
+runPart2 path = do
+  input <- readInput2 path
   return $ solve input
